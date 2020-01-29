fc3bef1635c8f7d67585fd158b9d796b54344a7d
codekaust <kaustubh1303@gmail.com>
2020-01-29 19:31:07 +0530

    can now push and see the contents of db, working GET and POST

--- Flask/Flask-SQLAlchemy/app.py
+++ Flask/Flask-SQLAlchemy/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -14,9 +14,23 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Can't add task."
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks = tasks)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
