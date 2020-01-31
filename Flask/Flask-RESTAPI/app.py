b29fa3ded35b411b707fb45b890c4b72feed80f1
codekaust <kaustubh1303@gmail.com>
2020-01-31 13:38:13 +0530

    added route to add product in db

--- Flask/Flask-RESTAPI/app.py
+++ Flask/Flask-RESTAPI/app.py
@@ -20,7 +20,7 @@ class Product(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), unique=True)
   description = db.Column(db.String(200))
-  price = db.Column(db.Float)
+  price = db.Column(db.Integer)
   qty = db.Column(db.Integer)
 
   def __init__(self, name, description, price, qty):
@@ -37,6 +37,21 @@ class ProductSchema(ma.Schema):
 # Init schema
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
+
+# Create product
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    new_product = Product(name, description, price, qty)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
 
 # Run Server
 if __name__=='__main__':
