19456e2cbf9e9fef3f5b94a50a79b9d5072c59eb
codekaust <kaustubh1303@gmail.com>
2020-01-31 13:22:51 +0530

    remove arg strict=True as it is not longer required: https://teratail.com/questions/210241

--- Flask/Flask-RESTAPI/app.py
+++ Flask/Flask-RESTAPI/app.py
@@ -15,28 +15,28 @@ db = SQLAlchemy(app)
 # Init Marshmallow
 ma = Marshmallow(app)
 
-# Product Class/Model (DB MODEL)
+# Product Class/Model (db model)
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    description = db.Column(db.String(200))
-    price = db.Column(db.Float)
-    qty = db.Column(db.Integer)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), unique=True)
+  description = db.Column(db.String(200))
+  price = db.Column(db.Float)
+  qty = db.Column(db.Integer)
 
-    def __init__(self, name, description, price, qty):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.qty = qty
+  def __init__(self, name, description, price, qty):
+    self.name = name
+    self.description = description
+    self.price = price
+    self.qty = qty
 
 # Product Schema
 class ProductSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'name', 'description', 'price', 'qty')
+  class Meta:
+    fields = ('id', 'name', 'description', 'price', 'qty')
 
-# Init Schema
-product_schema = ProductSchema(strict=True)
-products_schema = ProductSchema(many=True, strict=True)
+# Init schema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
 # Run Server
 if __name__=='__main__':
