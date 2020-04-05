209be0f1ec446ec084073a60bb1ae4850f5deb3b
codekaust <kaustubh1303@gmail.com>
2020-04-05 18:24:00 +0530

    attempt at client-server chat

---
+++ SocketProgrammingC++/single-client-server-chat/client.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#define PORT 8080 
+
+#include <iostream>
+#include <string>
+
+int main(){
+    int sock=0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    if((sock = socket(AF_INET, SOCK_STREAM, 0))<0){
+        printf("\n Socket creation error \n"); 
+		return -1; 
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+    while(true){
+        int valread;
+
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+        { 
+            printf("\nConnection Failed \n"); 
+            return -1; 
+        } 
+        
+        std::string in;
+        std::getline(std::cin, in);
+
+        if(in=="exit"){
+            break;
+        }
+
+        char* request  = (char *)(in.c_str());
+
+        send(sock, request, strlen(request), 0);
+        valread = read(sock, buffer, 1024);
+        std::cout<<buffer<<std::endl;
+    }
+    return 0;
+}
